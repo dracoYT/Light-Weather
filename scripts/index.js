b031ed0c731a0705af1be18f215b35eb7309e14d
draco <44122691+dracoYT@users.noreply.github.com>
2020-09-09 21:56:48 -0500

    Uploaded - HTML - CSS - JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,8 @@
 console.log(`
 Air fetch Cosole is not avalible\nWait 1 week more to use it, it still in development`);
-
+var data = InputEvent;
+function fetch() {
+    if (fetch.data = arduino) {
+        
+    }
+}
